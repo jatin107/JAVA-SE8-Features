10d5e78c30a49c165fb203e1f19e4095c0a1bec8
jatin107 <jatingoyal100@gmail.com>
2017-05-15 07:41:24 +0530

    ok

--- JAVA8Features/src/lambda/UseSimpleInterface.java
+++ JAVA8Features/src/lambda/UseSimpleInterface.java
@@ -3,7 +3,8 @@ package lambda;
 public class UseSimpleInterface {
 
 	public static void main(String[] args) {
-		
+
+		System.out.println("ok");
 		SimpleInterface obj=()->{System.out.println("test");};
 		obj.doSomething();
 		SimpleInterfacewithArgs obj1=(v1,v2)->{System.out.println(v1*v2);};
